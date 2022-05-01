c78c6b2b67f5bb07c5885025e3c347f2bf512efa
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-01 19:16:35 +0530

    Added: FibonacciSeries

---
+++ Day-6-Logical Problems/FindFibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6_Logical_Problems
+{
+    internal class FindFibonacci
+    {
+        public int n1 = 0;
+        public int n2 = 1;
+
+
+        public void  FindFibo()
+        {
+            int i,n3;
+            for(i=1;i<=10;i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3);
+                if(i >0 && i<10)
+                {
+                    Console.Write(",");
+                }
+                n1 = n2;
+                n2 = n3;
+            }
+            
+        }
+    }
+}
---
+++ Day-6-Logical Problems/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Day_6_Logical_Problems;
+
+FindFibonacci obj = new FindFibonacci();
+Console.Write("Fibonacci Series:- ");
+obj.FindFibo();
+
+
+
