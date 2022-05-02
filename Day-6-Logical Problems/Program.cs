799a3c08e2a3674000a2a7696f8bc561f4263d45
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-02 15:49:36 +0530

    Added: CouponNumber

--- Day-6-Logical Problems/Program.cs
+++ Day-6-Logical Problems/Program.cs
@@ -1,13 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
 using Day_6_Logical_Problems;
 
-Console.WriteLine("---- Welcome To Number Operations ----\n");
-    
-Console.Write("Enter the number to Reverse :- ");
-int n = int.Parse(Console.ReadLine());
-FindReverseNo obj = new FindReverseNo();
-obj.FindReverse(n);
-
-
+FindCouponNo obj = new FindCouponNo();
+obj.FindCouponNumber();
